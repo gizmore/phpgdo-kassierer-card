0a013ae647c5e9aad81b213eba944b7716f180d9
gizmore <gizmore@wechall.net>
2022-09-07 05:20:49 +0200

    '7.0.1-r1569 kc'

--- lang/faq_en.php
+++ lang/faq_en.php
@@ -14,7 +14,23 @@ return [
 		],
 		[
 			'title' => 'How do i collect bonus points?',
-			'infos' => 'The employees shall be able to get free food and drinks in their working locagtions, in exchange for our bonus points.',
+			'infos' => 'Make your customers print Kassierercard Coupons and hand them to you.',
+		],
+		[
+			'title' => 'How does this project get funded?',
+			'infos' => 'It currently does not. We are looking for partners!',
+		],
+		[
+			'title' => 'How do you detect abuse?',
+			'infos' => 'We are using cookies and IP checks to detect double accounts. In the end it is just about some cake and coffee?',
+		],
+		[
+			'title' => 'The idea is horrible. How come?',
+			'infos' => 'I always liked the idea and it\'s a test case for my programming project.',
+		],
+		[
+			'title' => 'The idea is great! How come?',
+			'infos' => 'Thank you!',
 		],
 		
 	],
