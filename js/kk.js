59edc382355fe82f3545c8af8ec01577dd037d0a
gizmore <gizmore@wechall.net>
2022-09-30 16:56:38 +0200

    '7.0.1-r1629 js err'

--- js/kk.js
+++ js/kk.js
@@ -6,7 +6,7 @@ window.KC.enhanceCouponTokens = function() {
 	$('.kc-coupon-token').each(function() {
 		let cont = $(this);
 //		let conf = cont.attr('data-config');
-		conf = {length:10}; 
+		let conf = {length:10}; 
 		let name = cont.find('input').hide().attr('name');
 		for (let i = conf.length; i >= 0; i--)  {
 			cont.prepend('<input name="'+name+'_'+i+'" />');
