5bef0c3afc5508456c3389271509289949fdf9bb
gizmore <gizmore@wechall.net>
2022-10-21 12:28:39 +0200

    '7.0.1-r1672 KC FAQ'

--- lang/faq_de.php
+++ lang/faq_de.php
@@ -13,7 +13,7 @@ Wir sind transparent und unabhängig und haben für die Zukunft gewisse Wohltät
 Auch wir wollen was bewegen!<br/>
 Bitte helfen Sie uns, den Menschen etwas Macht zu bringen.<br/>',
 
-	'kk_faq_t1c' => 'Wie finanzieren Sie das?',	'kk_faq_b1c' => 'Wir versuchen, schnell eine kritische Masse und eine unterhaltsame Seite zu erreichen.<br/>bis dahin sind die kosten knapp kalkuliert und werden von einem typ namens gizmore gedeckt :)<br/>
+	'kk_faq_t1c' => 'Wie finanzieren Sie das?',	'kk_faq_b1c' => 'Wir versuchen, schnell eine kritische Masse und eine unterhaltsame Seite zu erreichen.<br/>bis dahin sind die Kosten knapp kalkuliert und werden von einem Typen namens gizmore gedeckt :)<br/>
 Aktuell haben wir 0 Ausgaben für Server und Programmierung,
 und 100 % des Marketingkapitals endet in den Angeboten; Naja, außer ein paar Bierchen mit dem Team.<br/>
 Er hofft, etwas Freude und ein Lächeln in Ihren Arbeitstag zu bringen.',
@@ -29,67 +29,92 @@ Wenn Sie an Computersicherheit interessiert sind, sollten Sie %s auschecken.',
 
     	'kk_faq_t2' => 'Wie verdienen wir Punkte?',	'kk_faq_b2' => 'Es gibt drei Arten von Konten.<br/>Kunde, Kassierer und Unternehmen.
 Nennen wir sie die drei C\'s; CEO, Christian und Charlette.<br/>
-Kunden können Coupons ausdrucken (und aufschreiben).<br/>
-Die Kassierer und Arbeiter können sie betreten, um Sterne zu sammeln.<br/>
-Kunden erhalten auch einige Sterne (Diamanten), wenn sie Leute einladen oder den Arbeitern Karten geben.<br/>
-Die Firmen bieten Waren für die Stars an und bekommen dafür auch Diamanten.
-Diese sind für Wettbewerbe und Ranglisten.',
-
-    	'kk_faq_t3' => 'Der Kunde 1x1',	'kk_faq_b3' => 'Als Kunde;Drucken Sie Coupons aus und geben Sie sie an die Kassierer und Mitarbeiter Ihrer Wahl weiter.<br/>
-Sie können jeden Tag %s kostenlose Sterne drucken.<br/>
-Sie können entweder einen Coupon ausdrucken, den Code aufschreiben,
-oder verwenden Sie Mobiltelefone, um Codes an die Arbeiter zu übertragen.<br/>
+Kunden können Coupons ausdrucken (oder auch abschreiben).<br/>
+Die Kassierer und Arbeiter können diese eingeben, um Sterne zu sammeln.<br/>
+Kunden erhalten auch Sterne (Diamanten), wenn sie Leute einladen, oder den Arbeitern die Coupons geben.<br/>
+Die Firmen bieten Waren für die Sterne und Diamanten an,
+und bekommen dafür ebenfalls Diamanten.
+Bei den Unternemen sind diese sind für Wettbewerbe und Besten-Listen interessant.<br/>
+Die Kunden erhalten die gleichen Angebote wie die Arbeiter,
+nur halt für Diamanten anstatt Sterne.',
+	
+    	'kk_faq_t3' => 'Das Kunden Einmaleins',	'kk_faq_b3' => 'Als Kunde drucken Sie Coupons aus und 
+verteilen diesen an die Arbeiter.
+Auch das werben weiterer Kunden bringt Diamanten.<br/>
+Sie können jeden Tag %s kostenlose Sterne drucken und verteilen.<br/>
+Sie können die Coupons ausdrucken, den Code aufschreiben,
+oder Ihr Mobiltelefon verwenden, um Codes an die Arbeiter zu übertragen.<br/>
 Sie erhalten einen Diamanten für jeden %s-ten Stern, den ein Arbeiter von Ihnen eingibt.',
 
-	'kk_faq_t4' => 'Die Kassenkrise',	'kk_faq_b4' => 'Vergessen wir nicht, dass wir das für Sie tun, liebe Arbeiter :).<br/>Derzeit können nur Administratoren Kassierer einzeln einladen, dies wird sich jedoch in Phase 2 ändern.<br/>
-Wir versuchen vorerst Peine und vielleicht noch ein oder zwei andere Städte im ersten Durchgang abzudecken.<br/>
-Wenn Ihnen die Idee gefällt, senden Sie Einladungen an Ihre Kunden. - Kunden benötigen keinen Anmeldecode.<br/>
-Sie erhalten Gutscheine von Ihren Kunden, die Sie gegen Waren der Unternehmen eintauschen können.<br/>
+	'kk_faq_t4' => 'Die Kassenkrise',	'kk_faq_b4' => 'Vergessen wir nicht, dass wir das für Euch tun, liebe Arbeiter und Angestellten :).<br/>Derzeit können nur Administratoren Kassierer einzeln einladen,
+dies wird sich jedoch in Phase 2 ändern.<br/>
+Wir versuchen vorerst Peine, und vielleicht noch ein oder zwei andere Städte, im ersten Durchgang ein wenig abzudecken.<br/>
+Wenn Ihnen die Idee gefällt, senden Sie Einladungen an weitere Kunden. - Kunden benötigen keinen Anmeldecode.<br/>
+Sie erhalten Coupons von Ihren Kunden, die Sie gegen Sterne, und schliesslich Waren der Werbepartner-Unternehmen einlösen können.<br/>
 Dies kann durch den Austausch eines Coupon-Codes oder das Scannen eines QR-Codes erfolgen.<br/>
-Wir hoffen, dass Ihnen das ein wenig Spaß macht!',
+Auch der simple Druck auf einen Knopf kann ausreichen,
+weil nicht jeder Werbepartner sein Handy am Arbeitsplatz nutzen kann.
+Naja, wir hoffen, dass Ihnen das auch ein wenig Spaß macht!<br/>
+Wir könn(t)en tolle Dinge mit einer freien Platform anfangen.',
 
-    	'kk_faq_t5' => 'Das Firmenkochbuch',	'kk_faq_b5' => 'Zu guter Letzt gibt es Firmen.<br/>Ihre Aufgabe ist es, Ihren Arbeitern ein bisschen Geld und Produkte zu geben.<br/>
-Im Gegenzug erhalten Sie 100 % Rabatt auf die Rückseite jeder bedruckten KassiererCard,<br/>
-eine Merchandise-Plattform, die nicht mit aufgeblähten Metriken vergiftet ist.<br/>',
+    'kk_faq_t5' => 'Das Firmenkochbuch',	'kk_faq_b5' => 'Zu guter Letzt gibt es ja auch die Firmen.<br/>Ihre Aufgabe ist es, Ihren Arbeitern ein bisschen, Geld vllt. nicht, aber Produkte zu geben.<br/>
+Im Gegenzug erhalten Sie 100 % Rabatt auf die Rückseite jeder bedruckten KassiererCard.<br/>
+Auch nehmt ihr automatisch an Wettbewerben teil, und könnt euch in der Community einbringen.<br/>
+eine Merchandise-Plattform, die nicht mit aufgeblähten Metriken vergiftet ist,
+sondern aus echten Peinern besteht.',
 
-    	'kk_faq_t6' => 'Pizza und Cola sind lahm, ich will bessere Sachen für meine Coupons!',	'kk_faq_b6' => 'Für jeden eingelösten Coupon erhöht sich Ihr Benutzerlevel um einen Punkt.<br/>Mit Ihrem Benutzerlevel können Sie Forenbeiträge verfassen,
-Links zu dieser Seite hinzufügen, Umfragen erstellen und andere Dinge tun.<br/>',
+    'kk_faq_t6' => 'Pizza und Cola sind lahm, ich will bessere Angebote für meine Sterne und Diamnten!',	'kk_faq_b6' => 'Für jeden eingelösten Coupon erhöht sich Ihr Benutzerlevel um einen Punkt.<br/>Mit Ihrem Benutzerlevel können Sie Forenbeiträge verfassen,
+Links zu dieser Seite hinzufügen, Umfragen erstellen und andere Dinge tun.<br/>
+<i>Diese Funktionen sind allerdings noch in Arbeit.</i>',
 
-    	'kk_faq_t7' => 'Kann ich Punkte an andere Benutzer übertragen?',	'kk_faq_b7' => 'Nein, du kannst nur Waren oder Rabatte bekommen,oder virtuelle Dinge wie das Erstellen von Umfragen oder Anzeigen.<br/>
+    	
+	'kk_faq_t7' => 'Kann ich Punkte an andere Benutzer übertragen?',	'kk_faq_b7' => 'Nein, zur Zeit nicht. Du kannst nur Waren oder Rabatte bekommen,oder virtuelle Dinge wie das Erstellen von Umfragen oder Anzeigen.<br/>
 Es ist auch geplant, NonFungebleAssets zu erstellen. Zum Beispiel für den Gewinn eines Gewinnspiels.<br/>
-Ideen willkommen!',
+Ideen willkommen!<br/>
+Auch das senden von Sternen direkt von Kunde zu Kassierer wäre denkbar,
+wir halten davon aber vorerst Abstand.<br/>',
     
-	'kk_faq_t8' => 'Die Seite ist hässlich und sieht aus als wäre es 1999!',	'kk_faq_b8' => 'Ich habe viel Mühe in das Design dieses Systems gesteckt.<br/>Das aktuelle Thema mag hässlich sein, aber es wird in Zukunft schöner sein.',
+	'kk_faq_t8' => 'Die Seite ist hässlich und sieht aus als wäre es 1999!',	'kk_faq_b8' => 'Ich habe viel Mühe in das Design dieses Systems gesteckt.<br/>Das aktuelle Design mag hässlich sein, aber es wird in Zukunft schöner werden.<br/>
+Außerdem waren die 90er zehnmal besser als heute!',
 
-	'kk_faq_t9' => 'Sie haben Dutzende von Rechtschreibfehlern, es ist mühsam, die Seite zu benutzen.',	'kk_faq_b9' => 'Ja, ich habe keinen Ritus!',	'kk_faq_t10' => 'Ich habe keinen Kontakt zu Kassen.',	'kk_faq_b10' => 'Kein Problem.<br/>Die CashierCard ist für jeden Mitarbeiter in jeder Branche geeignet.<br/>
-Laden Sie einfach mehr Leute ein und drucken Sie weiter und verbreiten Sie die Nachricht!',
+	'kk_faq_t9' => 'Sie haben dutzende von Rechtschreibfehlern, es ist mühsam, die Seite zu benutzen.',	'kk_faq_b9' => 'Ja, ich habe keinen Ritus!',
+		'kk_faq_t10' => 'Ich habe keinen Kontakt zu Kassierern.',	'kk_faq_b10' => 'Kein Problem.<br/>Die KassiererCard ist für jeden Arbeiter in einer jeden Branche gedacht.<br/>
+Wer natürlich nur sehr wenig Kundenkontakt hat ist vielleicht auch auf seinen Arbeitgeber angewiesen.
+Naja, laden Sie einfach mehr Leute ein und drucken Sie weiter und verbreiten Sie die Nachricht!',
 
-	'kk_faq_t11' => 'Ich habe eine tolle Idee!',	'kk_faq_b11' => 'Großartig, lass es uns wissen! Wir haben auch dutzende Ideen.<br/>
+	'kk_faq_t11' => 'Ich habe eine tolle Idee!',	'kk_faq_b11' => 'Großartig, lassen Sie uns wissen! Wir haben auch dutzende Ideen.<br/>
 Eine offizielle ToDo gibt es hier: %s.<br/>
-Sie können auch ein Ticket oder eine PullRequest auf %s öffnen,
-aber denken Sie daran, dass dies eine eigene Software ist!',
-	'kk_faq_lnk' => 'GitHub',	'kk_faq_t12' => 'Das ist alles so verwirrend...',	'kk_faq_b12' => 'Wenn ein Symbol golden ist, enthält es zusätzliche Informationen.<br/>Wenn etwas unklar ist, bitte %s.',
+Sie können auch ein Ticket oder eine Pull-Request auf %s eröffnen,
+aber denken Sie daran, dass dies eigene, nicht freie, Software ist!<br/>
+Sie ist halt nur Quelloffen. Das System bleibt natürlich unter unser aller Kontrolle.',
+	'kk_faq_lnk' => 'GitHub',
+	'kk_faq_t12' => 'Das ist alles so verwirrend...',	'kk_faq_b12' => 'Wenn ein Symbol golden ist, enthält es zusätzliche Informationen *verwirr*.<br/>Wenn etwas unklar ist, bitte %s.',
 
 	'kk_faq_t13' => 'Wie viele Karten kann ich drucken?',	'kk_faq_b13' => 'Karten können von Kunden, Managern, Partnern und höher gedruckt werden. Keine Kassierer.<br/>
-Als Kunde können Sie pro Zeitraum %s Sterne drucken.<br/>
-Eine Periode dauert 2 Tage, und es gibt 3 Perioden pro Woche; Mo/Di, Mi/Do und Fr/Sa.<br/>
-Das bedeutet, dass uns ein aktiver Kunde etwa %s pro Monat kosten könnte, aber sehen Sie sich die Statistiken im linken Menü an!<br/>
-Sonntags wird nicht gedruckt.<br/>
+Als Kunde können Sie pro Tag %s Sterne gratis drucken.<br/>
+Das bedeutet, dass uns ein aktiver Kunde etwa %s pro Monat kosten könnte,
+aber sehen Sie sich die Statistiken im linken Menü an!<br/>
+Sonntags wird nicht gedruckt!<br/>
 Partner und Manager haben natürlich Sonderregeln.',
 
-	'kk_faq_t14' => 'Wofür sind die Sterne und Diamanten?',	'kk_faq_b14' => 'Sterne können von Kunden aus dem Nichts erschaffen werden,aber sie können sie nicht verwenden, außer um sie Arbeitern zu geben.<br/>
-Wenn ein Arbeiter einen Gutschein von Ihnen eingibt, erhalten Sie Diamanten zurück.<br/>
-Diamanten haben für Kunden denselben Wert wie Sterne, da sie sie gegen Angebote eintauschen können.
-Diamanten bringen Sie auch in die Rangliste, wo wir einige Preise oder eine Tombola aufstellen können.<br/>
-Ein €uro entspricht ungefähr %s Sternen. Dieser Preis kann sich ändern!<br/>
+	'kk_faq_t14' => 'Wofür sind die Sterne und Diamanten?',	'kk_faq_b14' => 'Sterne können von Kunden aus dem Nichts erschaffen werden,aber sie können sie nicht verwenden, außer um sie den Arbeitern zu geben.<br/>
+Wenn ein Arbeiter einen Gutschein von Ihnen eingibt, erhalten Sie Diamanten dafür.<br/>
+Diamanten haben für Kunden denselben Wert wie Sterne,
+da sie sie gegen Angebote eintauschen lassen.
+Diamanten bringen Sie auch in die Rangliste,
+wo wir einige Preise oder eine Tombola aufstellen werden.<br/>
+Ein €uro entspricht ungefähr %s Sternen.
+Dieser Preis kann sich natürlich ändern!<br/>
 Außerdem können Sie die Sterne nur gegen die Angebote unserer Partner eintauschen.
 Diamanten werden auch für die Wettbewerbe verwendet,<br/>
-wie Firma, Kassierer, Kunde, Angebot, von, der, Monat.<br/>
-Jedes eingelöste Angebot bringt dem Unternehmen Diamanten ein,
+wie z.B. `Angebot der Woche` oder `Kassierer des Jahres`.<br/>
+Jedes eingelöste Angebot bringt den Unternehmen Diamanten ein,
 damit sie um das sozialste Unternehmen aller Zeiten konkurrieren können.<br/>
-Als Kunde und Kassierer können Sie Diamanten verdienen, indem Sie mehr Benutzer einladen.<br/>',
+Als Kunde und Kassierer können Sie auch Diamanten verdienen, indem Sie mehr Benutzer einladen.<br/>',
 
 	'kk_faq_t15' => 'Wie lange halten meine Sterne?',	'kk_faq_b15' => 'Die Lebensdauer Ihrer Sterne ist unbegrenzt.<br/>Sofern Sie nicht betrügen, unterstützen wir Sie gerne!',
 
-
+	'kk_faq_t16' => 'Klingt utopisch. Machst Du Witze?',
+	'kk_faq_b16' => ':)',
+	
 ];
