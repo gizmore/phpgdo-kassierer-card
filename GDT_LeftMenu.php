2d9bcfbd48cd639309490f559ee5046aa6fed1b7
gizmore <gizmore@wechall.net>
2022-10-31 12:51:13 +0100

    '7.0.1-r1690'

--- GDT_LeftMenu.php
+++ GDT_LeftMenu.php
@@ -24,7 +24,7 @@ final class GDT_LeftMenu extends GDT_Menu
 			GDT_Link::make()->href($mod->href('Offers'))->text('link_kk_offers', [KC_Offer::getAvailableOffers()])->icon('star'),
 			GDT_Link::make('link_kk_companys')->href($mod->href('Partners'))->textArgs(KC_Partner::numTotal())->icon('icecream'),
 			GDT_Link::make()->href($mod->href('Competitions'))->icon('trophy')->text('link_kk_competitions', [count(GDT_CompetitionSection::make()->enumValues)]),
-			GDT_Link::make()->href($mod->href('Favorites'))->icon('heart')->text('link_kk_favorites', [count(GDT_CompetitionSection::make()->enumValues)]),
+			GDT_Link::make()->href($mod->href('Favorites'))->icon('heart')->text('link_kk_favorites', [count(GDT_FavSection::make()->enumValues)]),
 			GDT_Link::make()->href($mod->href('Statistics'))->text('link_kk_statistics', [777])->icon('amt'),
 			GDT_Link::make('link_kk_businesses')->href($mod->href('Businesses'))->textArgs(KC_Business::numTotal())->icon('house'),
 			GDT_Link::make('link_kk_employees')->href($mod->href('Employees'))->textArgs(KC_Working::numEmployeesTotal())->icon('work'),
