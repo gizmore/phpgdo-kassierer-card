66fa9de4e656f22854e37aa1eccbadde72671272
gizmore <gizmore@wechall.net>
2026-09-18 04:08:40 +0200

    'more korean'

---
+++ lang/kassierercard_ko.php
@@ -0,0 +1,367 @@
+<?php
+namespace GDO\KassiererCard\lang;
+
+return [
+	'module_kassierercard' => '계산원 카드',
+	'sitename' => 'KassiererCard.org',
+	'link_kk_home' => 'KassiererCard.org',
+	'link_kk_offers' => '%s 제안',
+	'link_kk_companys' => '%s 파트너',
+	'link_kk_statistics' => '%s 통계',
+	'link_kk_favorites' => '%s 즐겨찾기',
+	'link_kk_competitions' => '%s개의 경쟁',
+	'link_kk_businesses' => '%s 고용주',
+	'link_kk_employees' => '%s 직원',
+	'link_kk_team' => '%s 팀원',
+	'link_kk_help' => '도움말/FAQ',
+	'link_kk_company_page' => '파트너 페이지',
+	'link_kk_company_edit' => '편집',
+	'link_kk_company_enter' => '제안 코드를 입력하세요',
+	'link_kk_company_scans' => '제안 코드 스캔',
+	'link_kk_company_offers' => '귀하의 제안',
+	'kk_admin' => 'KC-직원',
+	'cfg_free_stars_period' => '기간별 무료 별',
+	'cfg_token_request_amt' => '쿠폰 입력: 시도',
+	'cfg_token_request_time' => '쿠폰 입력: 시간 초과',
+	'cfg_diamonds_created' => '다이아몬드 생성',
+	'cfg_diamonds_earned' => '획득한 다이아몬드',
+	'cfg_coupons_created' => '쿠폰이 생성되었습니다.',
+	'cfg_coupons_entered' => '쿠폰이 입력되었습니다',
+	'cfg_coupons_printed' => '쿠폰이 인쇄되었습니다',
+	'cfg_coupons_redeemed' => '사용된 쿠폰',
+	'cfg_stars_available' => '별표 사용 가능',
+	'cfg_stars_created' => '생성된 별',
+	'cfg_stars_earned' => '획득한 별',
+	'cfg_stars_entered' => '별 입력',
+	'cfg_stars_invited' => '스타를 초대했습니다',
+	'cfg_stars_purchased' => '구매한 별',
+	'cfg_stars_redeemed' => '별을 되찾았습니다',
+	'cfg_offers_created' => '제안이 생성되었습니다.',
+	'cfg_offers_redeemed' => '사용된 쿠폰',
+	'cfg_offers_fullfilled' => '제안이 완료되었습니다.',
+	'cfg_euros_invested' => '투자된 유로',
+	'cfg_euros_generated' => '유로 생성',
+	'cfg_euros_earned' => '유로 모금',
+	'cfg_euros_donated' => '기부된 유로',
+	'cfg_euros_revenue' => '유로 수익',
+	'cfg_users_invited' => '초대된 사용자',
+	'cfg_your_dream' => '',
+	'tt_cfg_diamonds_created' => '회사는 제안을 이행하여 다이아몬드를 만듭니다.',
+	'tt_cfg_diamonds_earned' => '고객과 계산원은 초대를 통해 다이아몬드를 얻습니다.',
+	'tt_cfg_coupons_created' => '고객은 계산원을 위한 쿠폰을 생성합니다.',
+	'tt_cfg_coupons_entered' => '계산원은 별을 얻기 위해 쿠폰을 입력합니다. 고객은 이를 통해 다이아몬드와 별을 얻습니다.',
+	'tt_cfg_coupons_printed' => '고객은 계산원을 위해 쿠폰을 인쇄합니다.',
+	'tt_cfg_coupons_redeemed' => '고객과 계산원은 쿠폰을 사용합니다.',
+	'tt_cfg_stars_available' => '별표는 쿠폰을 교환하는 데 사용할 수 있습니다.',
+	'tt_cfg_stars_created' => '고객은 별을 사용하여 쿠폰을 만들 수 있습니다.',
+	'tt_cfg_stars_purchased' => '고객은 실제 돈으로 스타를 구매할 수 있습니다.',
+	'tt_cfg_stars_redeemed' => '별을 되찾았습니다',
+	'tt_cfg_offers_created' => '제안이 생성되었습니다.',
+	'tt_cfg_offers_redeemed' => '사용된 쿠폰',
+	'tt_cfg_offers_fullfilled' => '제안이 완료되었습니다.',
+	'tt_cfg_euros_invested' => '총 유로 투자',
+	'tt_cfg_euros_fullfilled' => '창출된 총 유로 부',
+	'tt_cfg_euros_donated' => '회원들이 KC.org에 기부한 유로의 총액입니다.',
+	'tt_cfg_euros_earned' => '총 유로 가치가 모였습니다.',
+	'tt_cfg_euros_revenue' => 'KassiererCard.org의 총 유로 수익입니다.',
+	'tt_cfg_token_request_time' => '기간: 쿠폰 토큰을 차단하는 데 걸리는 시간은 얼마나 되나요?',
+	'grant_stars' => '그랜트 스타즈',
+	'msg_stars_granted' => '%s개의 별을 %s에게 부여했습니다. 그들에게 이메일이 전송되었습니다.',
+	'mail_subj_kk_grant_stars' => '%s: %s개의 별 부여됨',
+	'mail_body_kk_grant_stars' => '친애하는 %s 님
+
+%s 관리자가 %s에 %s개의 별을 부여했습니다.
+
+이유: %s.
+
+감사합니다,
+%4$s 팀',
+	'btn_stopped_there' => '나는 거기서 그만뒀다!',
+	'btn_working_there' => '나는 거기에서 일하고 있습니다!',
+	'generate_signup_code' => '가입 코드 생성',
+	'signup_codes' => '가입 코드',
+	'register_as_customer' => '고객으로 등록',
+	'mt_kassierercard_admincreatesignupcode' => '가입 코드 생성',
+	'list_kassierercard_adminsignupcodes' => '%s 가입 코드',
+	'msg_signup_code_created' => '%s에 대한 가입 코드가 생성되었습니다.',
+	'err_kk_no_coupon' => '이 방법으로는 쿠폰을 생성할 수 없습니다!',
+	'kk_invite_slogan' => '%s님으로 초대',
+	'msg_signup_stars' => '%s에 오신 것을 환영합니다. 초대로 %s개의 별을 획득했으며 이제 %s개의 별을 획득했습니다.',
+	'kc_info' => '초대 메모',
+	'kc_entered' => '입력 시간',
+	'msg_signup_customer_no_token' => '회원가입코드를 입력하지 않으셨기 때문에 고객으로 등록되어 있습니다.<br/>직장인이나 기업인 경우에는 연락주시기 바랍니다.<br/>재가입시 차단될 수 있습니다.',
+	'err_kk_signup_code_required' => '고객이 아닌 이상 가입 코드가 필요합니다.',
+	'msg_kk_free_customer_stars' => '오늘 %s개의 별을 받았습니다.',
+	'kk_type' => '계정 유형',
+	'kk_stars' => 'KC스타즈',
+	'kk_coupon' => '쿠폰',
+	'kk_cashier' => '계산원',
+	'kk_company' => '회사',
+	'kk_customer' => '고객',
+	'perm_kk_cashier' => '계산원',
+	'perm_kk_company' => '회사',
+	'perm_kk_customer' => '고객',
+	'perm_kk_distributor' => '대리점',
+	'perm_kk_manager' => '관리자',
+	'lbl_choose_account_type' => '계정 유형 선택',
+	'business' => '비즈니스',
+	'partner' => '파트너',
+	'slogan' => '슬로건',
+	'kc_token' => '코드',
+	'kc_offer' => '쿠폰 제공',
+	'kc_enterer' => '입장',
+	'offers' => '제안',
+	'offers_available' => '이용 가능한 혜택',
+	'favorite_meal' => '좋아하는 식사',
+	'favorite_movie' => '좋아하는 영화',
+	'favorite_song' => '좋아하는 노래',
+	'favorite_website' => '즐겨찾는 웹사이트',
+	'num_biz' => '매장',
+	'profession' => '직업',
+	'personal_website' => '자신의 웹사이트',
+	'tt_offers_available' => '귀하에게 제공되는 제안의 양입니다.',
+	'kk_info_register' => 'KassiererCard.org에 오신 것을 진심으로 환영합니다. 고객인지, 계산원인지, 회사인지 알려주세요. 회사는 우리의 상품 파트너입니다. 계산원은 직원이고 고객은... 고객이며 쿠폰을 인쇄할 수 있습니다.',
+	'enum_customer' => '고객',
+	'enum_cashier' => '계산원',
+	'enum_partner' => '회사',
+	'lbl_kk_register_code' => '가입 코드',
+	'tt_kk_register_code' => '캐셔나 회사로 가입하는 경우 유효한 가입 코드가 필요합니다.',
+	'err_kk_signup_code' => '계산원 또는 회사로서 유효한 가입 코드가 필요합니다.',
+	'err_kk_signup_code_unknown' => '입력한 가입 코드를 알 수 없습니다.',
+	'err_kk_signup_code_type' => '잘못된 계정 유형!. 귀하의 가입 코드는 %s 전용입니다.',
+	'salary_gross' => '총 급여',
+	'salary_hourly' => '시간당 요금',
+	'favorite_artist' => '좋아하는 아티스트',
+	'favorite_book' => '좋아하는 책',
+	'your_dream' => '당신의 꿈',
+	'favorite_religion' => '좋아하는 종교',
+	'mt_kassierercard_welcome' => '환영합니다',
+	'md_kassierercard_welcome' => 'KassiererCard는 고객이 아닌 직원을 위한 보너스 프로그램입니다.',
+	'mt_kassierercard_partners' => '우리의 파트너',
+	'md_kassierercard_partners' => '직원을 위한 보너스 시스템인 KassiererCard.org의 파트너입니다.',
+	'md_kassierercard_businesses' => '이 목록은 귀하와의 거리순으로 정렬된 참여 업체의 개요입니다.',
+	'list_kassierercard_businesses' => '%s개의 기업',
+	'md_kassierercard_rateemployee' => '원하는 직원에게 쿠폰을 보내보세요. 하루에 최대 %s개의 쿠폰을 보낼 수 있습니다.',
+	'mt_kassierercard_workingthere' => '거기서 일하시나요?',
+	'md_kassierercard_workingthere' => 'KassiererCard.org에 순위가 매겨지도록 매장이나 직장을 설정하세요.',
+	'msg_kk_started_work' => '%s에서 %s에 근무를 시작하셨습니다.',
+	'msg_kk_stopped_work' => '%s에서 일을 멈췄습니다.',
+	'kc_partner' => '파트너',
+	'create_company' => '파트너 생성',
+	'list_kassierercard_partners' => '%s 파트너',
+	'kk_info_partners_table' => '상품 파트너는 쿠폰과 교환하여 상품을 제공합니다.',
+	'footer_partner' => '%s을(를) 통해 이 파트너에게 연락할 수 있습니다.',
+	'footer_partner_offers' => '이 파트너는 <a href="%s">%s개의 제안</a>을 가지고 있습니다.',
+	'info_kk_company_admin' => '여기에서 %s의 파트너가 되는 방법을 알아볼 수 있습니다.<br/>
+myspace처럼 자신만의 작은 웹페이지를 설정할 수 있습니다.<br/>
+귀하의 임무는 직원과 고객을 위한 제안을 상환하는 것입니다.<br/>
+QR 코드를 스캔하거나 직원의 휴대폰에 있는 버튼을 누르거나 코드를 입력하면 됩니다.<br/>
+<br/>
+이 작은 프로젝트에 참여해주셔서 진심으로 감사드립니다!<br/>
+ - 기즈모어',
+	'mt_kassierercard_partneredit' => '여기에서 현재 상태를 편집하세요',
+	'kk_info_partner_edit' => '여기에서 %s의 파트너 세부정보를 편집할 수 있습니다.<br/>
+<a href="%s">프로필을 편집</a>할 수도 있습니다.
+그렇게 하려면 오른쪽 메뉴에서 "계정"을 누르세요.<br/>
+질문이나 우려 사항이 있는 경우 <a href="%s">저희에게 연락</a>해 주세요.',
+	'list_kassierercard_partneroffers' => '%s 제안',
+	'err_kk_customer_cannot_afford_offer' => '고객 %s은(는) 귀하의 제안을 사용할 수 없습니다: %s',
+	'err_kk_redeem_hashcode' => '해시코드가 잘못되었습니다.<br/>해시코드를 크랙하는 방법은 GDOv7 문서를 읽어보세요.',
+	'enter_coupon' => '쿠폰 입력',
+	'err_kk_coupon_unknown' => '알 수 없는 쿠폰이거나 이미 입력된 쿠폰입니다.',
+	'msg_entered_stars' => '입력한 코드가 유효하며 %s개의 별을 획득했습니다.',
+	'err_kk_coupon_used' => '입력한 코드는 이미 입력되었습니다.',
+	'err_kc_token_tries' => '너무 많은 코드를 너무 빨리 입력했습니다. 잠시 기다려 주세요 %s.',
+	'entered_coupons' => '입력된 쿠폰',
+	'list_kassierercard_enteredcoupons' => '%s 쿠폰을 입력했습니다.',
+	'kk_info_offers' => '여기에서 해당 지역에서 사용 가능한 쿠폰을 찾을 수 있습니다.<br/>혜택은 파트너가 생성하며, 고객이 쿠폰을 인쇄하면 직원이 이를 사용합니다.<br/>사용된 쿠폰 5개마다 고객도 쿠폰을 받습니다.',
+	'create_offer' => '제안 만들기',
+	'md_kassierercard_offers' => 'KassiererCard.org에서 사용 가능한 모든 쿠폰 제안과 과거 쿠폰 제안을 표시합니다.',
+	'info_create_offer' => '새로운 쿠폰 유형/제안을 생성하세요.',
+	'list_kassierercard_offers' => '%s 쿠폰 유형',
+	'err_kk_offer_timeout' => '이 쿠폰의 혜택은 %s에 만료되어 더 이상 사용할 수 없습니다.',
+	'err_kk_offer_afford' => '당신은 이것을 상환할 여유가 없습니다. 제안에는 %s개의 별표가 필요하며 %s개의 별표만 사용할 수 있습니다.',
+	'err_kk_offer_no_more_for_you' => '이 제안에 대해 %s/%s 중 %s/%s 항목을 사용하셨습니다. 더 이상 사용할 수 없습니다.',
+	'kk_offer_status' => '총 %s개 중 %s개의 항목이 사용되었습니다. %s까지 이 쿠폰을 %s번 사용할 수 있습니다. 이 제안의 가격은 %s개의 쿠폰 별입니다.',
+	'create_coupon' => '쿠폰 생성',
+	'generate_coupons' => '쿠폰 생성',
+	'kk_info_create_coupon' => '쿠폰에 사용할 별 개수(%s개)를 선택하세요.<br/>선택적으로 뒷면에 대한 혜택을 선택하세요.',
+	'sel_coupon_offer' => '제안 선택',
+	'tt_create_offer' => '이 제안에 대한 쿠폰을 만들어 현지 직원과 공유하세요.',
+	'msg_coupon_created' => '캐셔카드 쿠폰이 생성되었습니다.',
+	'err_kk_create_stars' => '별 %s개를 만들고 싶지만 이 기간에는 별 %s개만 더 추가할 수 있습니다. 이 기간에 이미 %s개의 별을 생성했습니다.',
+	'back_side' => '뒷면',
+	'front_side' => '전면',
+	'mt_kassierercard_printcoupon' => '쿠폰 인쇄',
+	'btn_qrcode' => 'QR 코드',
+	'kk_info_print_coupon' => '쿠폰 중 하나를 인쇄하세요.<br/>
+프린터로 보내거나 코드를 수동으로 적어두세요.
+또는 직원에게 QR 코드를 보여주십시오.',
+	'err_unknown_coupon' => '이 쿠폰은 존재하지 않거나 귀하의 것이 아닙니다.',
+	'msg_kk_enter_auth' => '인증되지 않았습니다.<br/>
+로그인한 후 다시 시도해 주세요.',
+	'err_print_sundays' => '일요일에는 쿠폰을 인쇄할 수 없습니다.<br/>
+내일 다시 시도해 보세요.',
+	'your_ad_here' => '파트너를 위한 뒷면 100%',
+	'link_preview_enter_coupon' => '쿠폰 URL',
+	'printed_coupons' => '인쇄된 쿠폰',
+	'li_kk_coupon_fresh' => '이 쿠폰은 최신이며 인쇄할 수 있습니다.',
+	'li_kk_coupon_printed' => '이 쿠폰은 %s에 인쇄되었습니다.',
+	'list_kassierercard_printedcoupons' => '%s 쿠폰이 준비되었습니다',
+	'redeem_offer' => '쿠폰 사용',
+	'all_offers' => '모든 제안',
+	'kk_info_redeem_offer' => '여기에서 쿠폰을 사용할 수 있습니다.<br/>
+왼쪽 메뉴에서 %s을(를) 찾을 수 있습니다.<br/>
+교환 과정을 중단할 수 없다는 점에 유의하세요.
+여기 버튼을 누른 후!',
+	'err_kk_offer_totaled' => '이 제안은 %s/%s번 사용되었으므로 완료되었습니다.',
+	'msg_redeem_started' => '당신은 지금 제안을 신청하고 있습니다!',
+	'kk_info_redeem_offer_now' => '당신은 <b>둘 중 하나</b>
+%s의 작업자가 확인 버튼을 누르도록 합니다.
+또는 QR 코드 버튼을 누르고 직원이 스캔하도록 하세요.<br/>
+어쨌든 돌아갈 길은 없어.
+제안을 청구할 때!',
+	'msg_redeem aborted' => '프로세스를 중단했습니다.',
+	'mt_kassierercard_partnerredeemqrcode' => '스캔 교환 코드',
+	'kk_info_redeem_qr' => '파트너로서 여기에서 고객의 QR 코드를 스캔하세요.',
+	'kk_info_redeem_offer_ok' => '지금 %s의 직원이 버튼을 누르도록 하세요!',
+	'msg_offer_redeemed_ok' => '이 쿠폰을 공식적으로 사용하셨습니다: %s.<br/>오류인 경우: 불운을 빕니다!',
+	'granted_coupons' => '교환된 쿠폰',
+	'list_kassierercard_employees' => '%s 직원',
+	'li_kk_working' => '%s는 %s부터 %s에서 일하고 있습니다.',
+	'li_kk_working_sub' => '%s개의 별을 수집하고 %s개의 쿠폰을 사용했습니다.',
+	'owner' => '소유자',
+	'create_business' => '고용주 만들기',
+	'kk_info_crud_business' => '여기에서 계산원이 있는 새 비즈니스를 추가할 수 있습니다.<br/>먼저 %s을(를) 원할 수도 있습니다.',
+	'biz_participating' => '이후 공식 고용주',
+	'biz_declining' => '우리를 거절했다',
+	'btn_biz_emplyoees' => '%s 직원',
+	'li_kk_coupon_entered' => '%s에 %s이(가) 입력했습니다.',
+	'the_team' => '팀',
+	'the_team_paragraph' => '이 페이지는 %s 팀원을 위한 것입니다.<br/>안녕하세요!',
+	'the_admins' => '관리자',
+	'the_admins_paragraph' => '%s 관리자는 모든 것에 액세스할 수 있습니다. 그들을 믿지 마세요!',
+	'the_staff' => '직원',
+	'the_staff_paragraph' => '%s 직원은 거의 모든 것에 액세스할 수 있습니다. 그들은 스팸을 검열하고 많은 이메일을 받습니다. 그들을 믿지 마세요!',
+	'the_kk_manager' => '관리자',
+	'the_kk_manager_paragraph' => '%s 관리자는 대리점에 인쇄된 카드와 전단지를 제공합니다.',
+	'the_kk_distributor' => '관리자',
+	'the_kk_distributor_paragraph' => '%s 판매점에서는 상품화 작업과 고객 플레이 테스트를 수행합니다.',
+	'invite_users' => '사람들 초대',
+	'kk_info_invite_stars' => '여기에서 더 많은 사람을 %s에 초대할 수 있습니다.
+초대장을 보내는 데는 %s/%s개의 별이 필요합니다.<br/>
+또한 초대장의 별 개수를 선택하고,
+초대받은 사람이 활성화되면,
+보상으로 다이아몬드를 얻습니다.
+그렇지 않으면 당신의 별은 사라지고 바람 속의 먼지가 됩니다!',
+	'msg_kk_sent_invitation' => '%s개의 별을 사용하여 %s에게 초대장을 보냈습니다. 또한 %s개의 별은 귀하가 그들에게 주는 선물이며, 그들이 우리에 가입하는 즉시 다이아몬드로 변환됩니다.',
+	'mailsubj_kk_invite' => '%s: %s로 초대',
+	'mailbody_kk_invite' => '안녕하세요 미래의 사용자 여러분,
+
+%s님이 당신을 %s에 %s로 초대했습니다.
+
+우리는 고객만을 위한 보너스 시스템(추가)이 아닌 근로자를 위한 시스템입니다.
+독립적이고 커피나 케밥과 같은 실제 상품을 제공합니다.
+
+환율은 1유로 == %s개 별입니다.
+
+%1$s(%s)이 환영 선물로 별 %s개를 보내드립니다.
+
+현재 케밥, 헤어컷, CBD 대마초 등과 같은 %s개의 매력적인 현지 제안이 있습니다.
+
+%s에서 확인해 보세요.
+
+감사합니다,
+%3$s 시스템',
+	'mail_subj_kk_invited_diamonds' => '%s: %s이(가) 합류했습니다!',
+	'mail_body_kk_invited_diamonds' => '친애하는 %s님,
+
+%s(%s)이(가) %s에 대한 귀하의 초대를 따르고 방금 활성화되었습니다.
+%s개의 별을 투자하여 %s개의 다이아몬드로 바뀌었습니다.
+현재 %s이(가) 있습니다.
+
+사용자 레벨이 %s만큼 증가하여 %s에 도달했습니다.
+
+감사합니다,
+%4$s 팀',
+	'responsible_is' => '이에 대한 책임은 다음과 같습니다.',
+	'kk_info_privacy_div' => 'KassiererCard.org는 귀하의 IP를 한동안 저장합니다.',
+	'pm_welcome_title' => '%s에 오신 것을 환영합니다',
+	'pm_welcome_message' => '안녕하세요 %s님,
+<br/>
+%s에 오신 것을 환영합니다.<br/>
+반대 방향으로 진행되는 보너스 시스템<br/>
+거꾸로, 머리 위로!<br/>
+<br/>
+사이트가 완전 새것이고,
+기능을 신속하게 구현하고 변경할 수 있습니다.<br/>
+사람과 자연에 더 많은 의미를 부여할 수 있도록 도와주세요.
+돈 대신.<br/>
+<br/>
+ - 기즈모어!',
+	'mailsubj_redeemed_staff' => '%s: 쿠폰이 사용되었습니다.',
+	'mailbody_redeemed_staff' => '안녕하세요 %s님,
+	
+사용자 %s이(가) 방금 다음 제안을 사용했습니다:
+	
+%s - %s
+
+이 쿠폰은 현재 %s/%s번 사용되었습니다.
+	
+우리는 당신에게 알려드립니다
+%s 시스템',
+	'mailsubj_redeemed_user' => '%s: 쿠폰이 사용되었습니다.',
+	'mailbody_redeemed_user' => '안녕하세요 %s님,
+	
+다음 제안에 대해 %s개의 별을 사용하셨습니다:
+
+%s - %s
+
+이제 %s개의 별이 남았습니다.
+
+진심으로,
+%s 시스템',
+	'mailsubj_redeemed_partner' => '%s: 쿠폰이 사용되었습니다.',
+	'mailbody_redeemed_partner' => '안녕하세요 %s님,
+	
+사용자 %s이(가) 방금 귀하의 쿠폰 중 하나를 사용했습니다:
+
+%s - %s
+
+이 쿠폰은 현재 %s/%s번 사용되었습니다.
+	
+진심으로,
+%s 시스템',
+	'mail_subj_customer_stars' => '%s: %s개의 별 획득',
+	'mail_body_customer_stars' => '안녕하세요 %s님,
+
+사용자 %s이(가) 별 %s개 쿠폰을 입력했습니다.
+이로써 별 %s개도 획득했습니다.
+
+잘하셨어요!
+%s 시스템',
+	'enum_cashier-of-the-week' => '금주의 캐셔',
+	'enum_cashier-of-the-month' => '이달의 캐셔',
+	'enum_cashier-of-the-year' => '올해의 계산원',
+	'enum_customer-of-the-week' => '금주의 고객',
+	'enum_customer-of-the-month' => '이달의 고객',
+	'enum_customer-of-the-year' => '올해의 고객',
+	'enum_company-of-the-week' => '금주의 회사',
+	'enum_company-of-the-month' => '이달의 회사',
+	'enum_company-of-the-year' => '올해의 기업',
+	'enum_business-of-the-week' => '금주의 비즈니스',
+	'enum_business-of-the-month' => '이달의 사업',
+	'enum_business-of-the-year' => '올해의 비즈니스',
+	'enum_offer-of-the-week' => '금주의 제안',
+	'enum_offer-of-the-month' => '이달의 제안',
+	'enum_offer-of-the-year' => '올해의 제안',
+	'competition_table' => '%s; %s개 항목; 페이지 %s/%s',
+	'mt_kk_favorites' => '즐겨찾는 %s',
+	'tt_kk_favorites' => '%s 즐겨찾는 "%s" 설정',
+	'md_kk_favorites' => '%s이(가) %s에서 가장 좋아하는 %s입니다. 실제 사용자 콘텐츠, 좋아하는 %2$s에 대한 실제 의견.',
+	'list_kassierercard_grantedcoupons' => '%s 에인게베네 쿠폰',
+	'md_kassierercard_grantedcoupons' => 'Übersicht ihrer erstellten 쿠폰, welche eingegben wurden.',
+	'mt_kassierercard_todo' => '할 일',
+	'mt_kassierercard_team' => '팀',
+	'kk_info_statistics' => '통계',
+];
